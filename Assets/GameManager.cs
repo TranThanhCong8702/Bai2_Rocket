ba816b54b91a5a34087cd9037529e635644469cf
congtran <congtran872002@gmail.com>
2024-04-29 01:13:49 +0700

    12-20 FPS

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Text FPS;
+    List<Box> boxes = new List<Box>();
     [SerializeField] GameObject cube;
     [SerializeField] Transform container;
     int dem = 0;
@@ -13,10 +16,62 @@ public class GameManager : MonoBehaviour
         {
             for (int j = -50; j < 50; j+=1)
             {
-                Instantiate(cube, new Vector3((float)i/10, 2f, (float)j / 10), Quaternion.identity, container);
+                var t = Instantiate(cube, new Vector3((float)i/10, 0, (float)j / 10), Quaternion.identity, container);
+                Box b= new Box();
+                b.GetI();
+                b.transform1 = t.transform;
+                boxes.Add(b);
                 dem++;
             }
         }
         Debug.Log(dem);
+    }
+
+    void CalculateFPS()
+    {
+        FPS.text = "FPS: " + (1 / Time.deltaTime).ToString();
+    }
+    private void Update()
+    {
+        CalculateFPS();
+    }
+    private void FixedUpdate()
+    {
+        foreach (var box in boxes)
+        {
+            box.Moving();
+        }
+    }
+
+}
+[System.Serializable]
+public class Box
+{
+    [SerializeField] float speed = 0.05f;
+    public Transform transform1;
+    int i = 0;
+    public void GetI()
+    {
+        i = Random.Range(0, 2);
+    }
+
+    public void Moving()
+    {
+        if (i == 0)
+        {
+            transform1.position += new Vector3(0, speed * Time.deltaTime, 0);
+            if (Mathf.Abs(transform1.position.y) > 0.075f * 2)
+            {
+                speed = -speed;
+            }
+        }
+        else
+        {
+            transform1.position -= new Vector3(0, speed * Time.deltaTime, 0);
+            if (Mathf.Abs(transform1.position.y) > 0.075f * 2)
+            {
+                speed = -speed;
+            }
+        }
     }
 }
